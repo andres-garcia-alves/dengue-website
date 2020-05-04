c8e30b1defe49c65bd8c0b80daadd65447692bbf
Andres Garcia Alves <andres_garcia_ao@hotmail.com>
2020-05-04 10:14:04 -0300

    project structure, web-components navigation-menu & footer-link

---
+++ resources/js/footer-links.js
@@ -0,0 +1,38 @@
+class FooterLinks extends HTMLElement {
+
+    content = `
+    
+        <!-- footer links -->
+        <div class="footer-container">
+
+            <div class="footer-title">
+                <h2>Mapa de Links</h2>
+            </div>
+
+            <div class="footer-links">
+                <p><a href="/autotest/autotest.html">Auto-Test</a></p>
+                <p><a href="/hospitales/hospitales.html">Hospitales</a></p>
+                <p><a href="/mapa/mapa.html">Mapa de Calor</a></p>
+                <p><a href="/noticias/noticias.html">Noticias</a></p>
+                <p><a href="/prevencion/prevencion.html">Prevención</a></p>
+                <p><a href="/sintomas/sintomas.html">Síntomas</a></p>
+                <p><a href="/telefonos/telefonos.html">Teléfonos Utiles</a></p>
+                <p><a href="/tratamientos/tratamientos.html">Tratamientos</a></p>
+            </div>
+        </div>
+
+        <!-- footer copyright -->
+        <footer class="footer-copyright">
+            <span>Copyright</span>
+            <i class="far fa-copyright"></i>
+            <span>2020 - Desarrollo de un Proyecto, CAECE</span>   
+        </footer>
+    `;
+
+    constructor() {
+        super();
+        this.innerHTML = this.content;
+    }
+}
+
+window.customElements.define('footer-links', FooterLinks);
---
+++ resources/js/navigation-menu.js
@@ -0,0 +1,47 @@
+class NavigationMenu extends HTMLElement {
+
+    content = `
+
+        <!-- burger button -->
+        <div class="menu-btn">
+            <i class="fas fa-bars fa-2x"></i>
+        </div>
+  
+        <!-- navigation main -->
+        <nav class="nav-main">
+
+            <!-- logo -->
+            <img src="/resources/img/brand.png" alt="TechNews Logo" class="nav-brand">
+            
+            <!-- navigation menu -->
+            <ul class="nav-menu">
+                <li><a href="/autotest/autotest.html">Auto-Test</a></li>
+                <li><a href="/hospitales/hospitales.html">Hospitales</a></li>
+                <li><a href="/mapa/mapa.html">Mapa de Calor</a></li>
+                <li><a href="/noticias/noticias.html">Noticias</a></li>
+                <li><a href="/prevencion/prevencion.html">Prevención</a></li>
+                <li><a href="/sintomas/sintomas.html">Síntomas</a></li>
+                <li><a href="/telefonos/telefonos.html">Tel. Utiles</a></li>
+                <li><a href="/tratamientos/tratamientos.html">Tratamientos</a></li>
+            </ul>
+
+            <!-- search -->
+            <ul class="nav-menu-right">
+                <li>
+                <a href="#">
+                    <i class="fas fa-search"></i>
+                </a>
+                </li>
+            </ul>
+            
+        </nav>
+        <hr>
+    `;
+
+    constructor() {
+        super();
+        this.innerHTML = this.content;
+    }
+}
+
+window.customElements.define('navigation-menu', NavigationMenu);
